b0842230640018bfe01795e1340bf8355740164b
VikaRazuiova <109959590+VikaRazuiova@users.noreply.github.com>
2022-09-06 07:26:31 +0000

    Deploying to gh-pages from @ VikaRazuiova/super-food@a3620e71e64a0f711b11a003581a3aa49bebec75 🚀

--- index.0461b1b0.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn1:document.querySelector("[data-modal-open]"),openModalBtn2:document.querySelector("[hero-modal-open]"),openModalBtn3:document.querySelector("[offers-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn1.addEventListener("click",o),e.openModalBtn2.addEventListener("click",o),e.openModalBtn3.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.0461b1b0.js.map
