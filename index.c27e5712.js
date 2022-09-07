3f833b51446d7b07bdc65cb2d9cfaeeb78d341c0
VikaRazuiova <109959590+VikaRazuiova@users.noreply.github.com>
2022-09-07 07:18:18 +0000

    Deploying to gh-pages from @ VikaRazuiova/super-food@2c95ffd32078ceb7b1ab2f9fbf3c5741d40c579c 🚀

---
+++ index.c27e5712.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",(function(){!function(){for(var t=document.querySelectorAll("form"),e=0;e<t.length;e++){function n(t){var e=t.querySelector("button[type=submit]");e&&e.addEventListener("click",(function(e){e.preventDefault(),t.classList.add("modal__form--submit-attempted")}))}n(t[e])}}()}));
+//# sourceMappingURL=index.c27e5712.js.map
